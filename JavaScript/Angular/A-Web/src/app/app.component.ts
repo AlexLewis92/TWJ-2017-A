eb11dfb86ca83d3166ca600013648dd08743f6f2
AlexLewis92 <USRDEL@USR17.local>
2017-05-17 14:36:47 -0500

    primer código en Angular

--- JavaScript/Angular/A-Web/src/app/app.component.ts
+++ JavaScript/Angular/A-Web/src/app/app.component.ts
@@ -1,21 +1,37 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'TWJ-A',
+  template: "<h1>hola Mundo</h1>",
   styleUrls: ['./app.component.css']
 })
 
+/*export class AppComponent {
+  title = 'app work!';
+}*/
 
 
-export class AppComponent {
-  title = 'Funciona!';
+export class AppComponent{
+  usuario:UsuarioInt={
+    nombre:"Dario",
+    apellido:" Naranjo"
+}
+constructor(){
+    console.log("constructor");
+    this.holaMundo();
+    console.log(this.saludar(this.usuario.nombre,
+    this.usuario.apellido))
+}
+holaMundo(){
+  console.log("Hola Mundo");
+}
+saludar(nombre:string,
+apellido?:string):string{
+  return `Hola ${nombre}${apellido}`;
+}
 }
 
-export class AppComponent1 {
-  nombre = 'hola!';
-}
-
-export class AppComponent2 {
-  apellido = 'patos!';
+interface UsuarioInt{
+  nombre:string,
+  apellido:string
 }
