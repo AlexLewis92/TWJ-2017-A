d5dd0846a3df0a09ad88e76c4de1a2f69824b11e
AlexLewis92 <USRDEL@USR17.local>
2017-05-31 14:43:42 -0500

    Planetas Star Wars

--- JavaScript/Angular/A-Web/src/app/Componentes/inicio/inicio.component.ts
+++ JavaScript/Angular/A-Web/src/app/Componentes/inicio/inicio.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/map';
+
+
 //comand+a y luego command command+alt+l
 @Component({
   selector: 'app-inicio',
@@ -8,6 +12,8 @@ import {Component, OnInit} from '@angular/core';
 export class InicioComponent implements OnInit {
 
   nombre: string = 'Dario';
+
+  planetas=[];
 
   arregloUsuarios = [
     {
@@ -22,14 +28,21 @@ export class InicioComponent implements OnInit {
       nombre: "Abdala",
       apellido: "Bucaran",
       conectado:false
+    },
+    {
+      nombre: "Juan",
+      apellido: "Flores",
+      conectado:true
     }
   ]
 
-  constructor() {
+  constructor(private _http:Http) {
+    //inicia la clase
+    //PERO EL COMPONENTE NO ESTA LISTO
+
   }
-
   ngOnInit() {
-
+  //AQUI SI ESTA LISTO EL COMPONENTE
   }
 
   cambiarNombre(): void {
@@ -47,5 +60,29 @@ export class InicioComponent implements OnInit {
 
   }
 
+  cargarPlanetas(){
+    this._http
+      .get("http://swapi.co/api/planets")
+      //.map(response=>response.json())
+      .subscribe(
+        (response)=>{
+          console.log("Response:",response);
+          console.log(response.json());
+          let respuesta=response.json();
+          console.log(respuesta.next);
+          this.planetas=respuesta.results;
+        },
+        (error)=>{
+          console.log("Response:",error);
+        },
+        ()=>{
+          console.log("finally");
+        }
+      )
+  }
 
+}
+
+interface PlanetasStarWars{
+  name:string;
 }
