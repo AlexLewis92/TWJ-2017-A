1ab4b0bf34b16c4d6c31e8cdef48efb9f85b2510
noemi.uyaguari@epn.edu.ec <USRDEL@USR17.local>
2017-06-28 14:12:46 -0500

    Igualado Angular

--- sail-docker-rest/api/models/Mascota.js
+++ sail-docker-rest/api/models/Mascota.js
@@ -1,23 +1,23 @@
 /**
- * Usuario.js
+ * Mascota.js
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
 module.exports = {
-  connection:'localDiskDb',
+
   attributes: {
-  nombre:{
-   type:'String'
- },
-    duenosMascotas:{
-      collection:"UsuarioMascota",
-      via:"idUsuario"
+    nombre:{
+      type:"string"
     },
-    correo:{
-    type:'email'
+    idRaza:{
+      model:"Raza"//Modelo a Relacionar (Papa)
+    },
+    duenosMascotas:{
+      collection:"UsuarioMascota",//Modelo
+      via:"idMascota"//Foreign Key
     }
+
   }
 };
-
--- sail-docker-rest/api/models/UsuarioMascota.js
+++ sail-docker-rest/api/models/UsuarioMascota.js
@@ -1,23 +1,26 @@
 /**
- * Usuario.js
+ * UsuarioMascota.js
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
+//DuenosDeMascota
+
 module.exports = {
-  connection:'localDiskDb',
+
   attributes: {
-  nombre:{
-   type:'String'
- },
-    duenosMascotas:{
-      collection:"UsuarioMascota",
-      via:"idUsuario"
+    idUsuario:{
+      model:"Usuario"//Tabla papa
     },
-    correo:{
-    type:'email'
+    idMascota:{
+      model:"Mascota"//Tabla papa
+    },
+    fechaDeInicioAdopcion:{
+      type:"date"
+    },
+    fechaDeFinalizacionAdopcion:{
+      type:"date"
     }
   }
 };
-
